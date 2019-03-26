b0291a8fb0e219dc2051fcad5a8fecf1934bbb59
Rauf Erk <erk.rauf@gmail.com>
2019-03-26 17:43:11 +0300

    array is inverted!

--- src/client/components/journal/index.js
+++ src/client/components/journal/index.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import { PAGES } from '../../routes/pages';
+import icon from '../../public/images/checkmark-16.png';
 export default class Journal extends Component {
   state = {
     tradesInfo: {
       tradesInfo: []
-
     }
   };
   fetchData = async () => {
     try {
       const dataFromBase = await fetch(PAGES.API.fetchData.path);
+      let tradesInfo = await dataFromBase.json()
+      tradesInfo.tradesInfo.sort((a,b) => b.number - a.number);
       this.setState({
-        tradesInfo: await dataFromBase.json()
+        tradesInfo
       })
     } catch (e) {
       console.error(e);
     }
   };
   componentDidMount() {
-    this.fetchData();
+    this.fetchData(); 
   }
   signalNames = ['sig 1', 'sig 2', 'sig 3', 'sig 4', 'sig 5', 'sig 6', 'sig 7', 'sig 8', 'sig 9', 'sig 10'];
   SignalHeader = this.signalNames.map((item) => {
@@ -26,7 +28,6 @@ export default class Journal extends Component {
       <th key={item} scope="col">{item}</th>
     );
   });
-
   render() {
     let { tradesInfo } = this.state.tradesInfo;
     return (
@@ -47,13 +48,13 @@ export default class Journal extends Component {
           <tbody>
             {tradesInfo.map(trade =>
               <tr key={trade.number}>
-                <td key={trade.number} >{trade.number}</td>
-                <td>{trade.tradeData}</td>
-                <td>{trade.security}</td>
-                <td>{trade.factor}</td>
-                <td>{trade.openPrice}</td>
-                <td>{trade.closePrice}</td>   
-                {trade.signals.map(signal => <td>{signal}</td>)}
+                <td key={trade.number+trade.number} >{trade.number}</td>
+                <td key={trade.number+trade.tradeData}>{trade.tradeData}</td>
+                <td key={trade.number+trade.security}>{trade.security}</td>
+                <td key={trade.number+trade.factor}>{trade.factor}</td>
+                <td key={trade.number+trade.openPrice}>{trade.openPrice}</td>
+                <td key={trade.number+trade.closePrice}>{trade.closePrice}</td>
+                {trade.signals.map(signal => <td >{signal ? <img key={trade.number + 'signal'} src={icon} alt="yes" /> : null}</td>)}
               </tr>
             )}
           </tbody>
