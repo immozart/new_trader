c9b012c210a81ceb7bcf370804f4b1dffbee170a
immozart <vadimpostoffice@mail.ru>
2019-03-28 11:15:59 +0300

    сохранение

--- src/client/components/journal/journal.js
+++ src/client/components/journal/journal.js
@@ -1,60 +1,65 @@
 import React, { Component } from 'react';
 import { PAGES } from '../../routes/pages';
 import icon from '../../public/images/checkmark-16.png';
-import './journal.css'
+import './journal.css';
 import { Redirect } from 'react-router-dom';
-export default class Journal extends Component {
 
+export default class Journal extends Component {
     state = {
-        tradesInfo: {
-            tradesInfo: []
-        }
+      tradesInfo: {
+        tradesInfo: []
+      }
     };
+
     fetchData = async () => {
-        try {
-            const dataFromBase = await fetch(PAGES.API.fetchData.path);
-            let tradesInfo = await dataFromBase.json()
-            tradesInfo.tradesInfo.sort((a, b) => b.number - a.number);
-            this.setState({
-                tradesInfo
-            })
-        } catch (e) {
-            console.error(e);
-        }
+      try {
+        const dataFromBase = await fetch(PAGES.API.fetchData.path);
+        const tradesInfo = await dataFromBase.json();
+        tradesInfo.tradesInfo.sort((a, b) => b.number - a.number);
+        this.setState({
+          tradesInfo
+        });
+      } catch (e) {
+        console.error(e);
+      }
     };
+
     componentDidMount() {
-        this.fetchData();
+        console.log(this.props)
+      this.fetchData();
     }
+
     signalNames = ['sig 1', 'sig 2', 'sig 3', 'sig 4', 'sig 5', 'sig 6', 'sig 7', 'sig 8', 'sig 9', 'sig 10'];
-    SignalHeader = this.signalNames.map((item) => {
-        return (
-            <th key={'SignalHeader' + item} scope="col">{item}</th>
-        );
-    });
+
+    SignalHeader = this.signalNames.map(item => (
+        <th key={`SignalHeader${item}`} scope="col">{item}</th>
+    ));
+
     RowOfCheckboxes = (keyForIndex) => {
-        for (let i = 0; i < 10; i++) {
-            return (
+      for (let i = 0; i < 10; i++) {
+        return (
                 <td scope="col">{keyForIndex}</td>
-            );
-        }
-    };
-    RenderChekboxes = this.signalNames.map((item) => {
-        return (
-            <div className="col" key={'Chekboxes' + item} >
-                <input type="text" className="form-control" placeholder={item} />
-            </div>
         );
-    });
+      }
+    };
+
+    RenderChekboxes = this.signalNames.map(item => (
+        <div className="col" key={`Chekboxes${item}`} >
+            <input type="text" className="form-control" placeholder={item} />
+        </div>
+    ));
+
     ParseDate = (dateTimeFromMongo) => {
-        let tmpData = new Date(dateTimeFromMongo);
-        return (
-            tmpData.toLocaleString()
-        );
+      const tmpData = new Date(dateTimeFromMongo);
+      return (
+        tmpData.toLocaleString()
+      );
     };
+
     render() {
-        let { tradesInfo } = this.state.tradesInfo;
-        let keyIndex = 0;
-        return (
+      const { tradesInfo } = this.state.tradesInfo;
+      let keyIndex = 0;
+      return (
             <div >
 
                 <h3><b><i>СДЕЛКИ</i></b></h3>
@@ -100,26 +105,29 @@ export default class Journal extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {tradesInfo.map(trade =>
-                            <tr key={trade.number} className='main-table-row'>
-                                <td>{trade.number}</td>
-                                <td >{this.ParseDate(trade.tradeData)}</td>
-                                <td >{trade.security}</td>
-                                <td >{trade.factor}</td>
-                                <td >{trade.openPrice}</td>
-                                <td >{trade.closePrice}</td>
-                                <td >{trade.result}</td>
-                                {trade.signals.map(signal => <td key={keyIndex++} >{signal ? <img src={icon} alt="yes" /> : null}</td>)}
-                                <td scope="col" >
-                                    <button type='button' className='btn btn-danger btn-sm'>
-                                        <i className='fa fa-trash-o' aria-hidden="true" />
-                                    </button>
-                                </td>
-                            </tr>
-                        )}
+                        {tradesInfo.map(trade => <tr key={trade.number} className='main-table-row'>
+                            <td>{trade.number}</td>
+                            <td >{this.ParseDate(trade.tradeData)}</td>
+                            <td >{trade.security}</td>
+                            <td >{trade.factor}</td>
+                            <td >{trade.openPrice}</td>
+                            <td >{trade.closePrice}</td>
+                            <td >{trade.result}</td>
+                            {trade.signals.map(signal => <td key={keyIndex++} >{signal ? <img src={icon} alt="yes" /> : null}</td>)}
+                            <td scope="col" >
+                                <button type='button' className='btn btn-danger btn-sm'>
+                                    <i className='fa fa-trash-o' aria-hidden="true" />
+                                </button>
+                            </td>
+                        </tr>)}
                     </tbody>
                 </table>
             </div >
-        );
+      );
     }
 }
+
+const mapStatetoProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+  });
