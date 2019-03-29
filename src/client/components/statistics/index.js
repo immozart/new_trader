05b482b2f74244b25c1adcf32b13f6e9bd8da7b3
bot <bank45@yandex.ru>
2019-03-29 15:35:54 +0300

    control signals update

--- src/client/components/statistics/index.js
+++ src/client/components/statistics/index.js
@@ -64,7 +64,6 @@ class Statistic extends Component {
         for (let i = 0; i < tradesInfo[j].signals.length; i++) {
           let sum = tradesInfo[j].signals[i] + signalsArr[i];
           signalsArr[i] = sum;
- 
         }
       }
     }
@@ -105,11 +104,9 @@ class Statistic extends Component {
     }
     //================================================
 
-
     const SecurityLabel = []
-    console.log('signalsArr=signalsArr', signalsArr)
+
     signalsArr = signalsArr.sort(function (a, b) { return b - a })
-    console.log('signalsArr=sort=signalsArr', signalsArr)
     for (let i in securityObj) {
       SecurityLabel.push(i)
       securityProfit.push(securityObj[i].value)
